9a9f47964675871160c754ddfc81ef8db3e6cad5
Kilian Sprotte <kilian.sprotte@gmail.com>
2024-05-19 00:19:08 +0200

    clisp-iterate-count-to-10/build.sh

--- clisp-iterate-count-to-10/build.sh
+++ clisp-iterate-count-to-10/build.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-mkdir -p $out/bin
-
 clisp -q -c $ITERATE/package.lisp -o out.fas
 cat out.fas >>iterate.fas
 
@@ -13,6 +11,7 @@ cat out.fas >>iterate.fas
 clisp -q -i iterate.fas -c $COUNT -o out.fas
 cat out.fas >>count.fas
 
+mkdir -p $out/bin
 echo "#!$(which clisp)" >$out/bin/clisp-iterate-count-to-10
 cat count.fas >>$out/bin/clisp-iterate-count-to-10
 chmod +x $out/bin/clisp-iterate-count-to-10
